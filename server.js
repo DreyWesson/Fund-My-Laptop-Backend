327c954da1c1494efbec089d05974201b3db53f6
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-15 09:39:37 +0100

    setup

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-require('dotenv').config();
 
 const app = express();
-const log = require('./src/utils/logger');
-
-const port = process.env.PORT || 3000;
 
 app.get('/', (res) => res.send('Setup!'));
 
-app.listen(port, () => log(`listening at ${port}`));
+module.exports = app;
