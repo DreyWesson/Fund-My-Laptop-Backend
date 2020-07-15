eb9ad8e0960bc2b5e8f531f98fe5292edf83aa07
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-15 10:36:50 +0100

    setup

--- bin/www.js
+++ bin/www.js
@@ -1,14 +1,17 @@
 const { createServer } = require('http');
 require('dotenv').config();
 const app = require('../server');
+const sequelize = require('../config/db');
 const logger = require('../utils/logger');
 
 const PORT = process.env.PORT || 3000;
 
 const server = createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
   logger.log(`Serving at http://localhost:${PORT} for ${process.env.NODE_ENV}`);
+  await sequelize.authenticate();
+  logger.log('Connection to database established successfully');
 });
 
 module.exports = server;
